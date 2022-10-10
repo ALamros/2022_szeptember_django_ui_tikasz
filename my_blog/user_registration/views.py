2caf1ab524d77be13de963dc5733b35a419d33e8
ALamros <tikasz.zsolt.szeb@hotmail.com>
2022-10-10 17:42:28 +0200

    profile

--- my_blog/user_registration/views.py
+++ my_blog/user_registration/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import ProfileModel
+from django.contrib.auth.models import User
 
 def register(request):
     print(request.method)
@@ -8,6 +11,10 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            usr_obj = User.objects.filter(username=username).first()
+            prof_temp = ProfileModel.objects.create(user_id=usr_obj.pk)
+
             messages.success(request, f"Your account has been created! ")
 
             return redirect('login')
@@ -18,3 +25,8 @@ def register(request):
         'form': form
     }
     return render(request, 'user/register.html', context=context)
+
+# @login_required(login_url='login')
+@login_required
+def profile_view(request):
+    return render(request, 'user/profile.html')

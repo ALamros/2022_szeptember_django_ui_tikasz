2261aaa2e3cdc87d56fcbc1ec46d44ead069458f
ALamros <tikasz.zsolt.szeb@hotmail.com>
2022-10-10 20:08:53 +0200

    finalize profile page

--- my_blog/user_registration/views.py
+++ my_blog/user_registration/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import ProfileModel
@@ -11,9 +11,9 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            usr_obj = User.objects.filter(username=username).first()
-            prof_temp = ProfileModel.objects.create(user_id=usr_obj.pk)
+            # username = form.cleaned_data.get('username')
+            # usr_obj = User.objects.filter(username=username).first()
+            # prof_temp = ProfileModel.objects.create(user_id=usr_obj.pk)
 
             messages.success(request, f"Your account has been created! ")
 
@@ -29,4 +29,20 @@ def register(request):
 # @login_required(login_url='login')
 @login_required
 def profile_view(request):
-    return render(request, 'user/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profilemodel)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, "Your account has been updated!")
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profilemodel)
+
+    context = {
+        "u_form":u_form,
+        "p_form":p_form
+    }
+    return render(request, 'user/profile.html', context=context)

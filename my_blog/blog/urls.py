332949d9ef8766201a1e06f5d1316a16f1fba4ea
ALamros <tikasz.zsolt.szeb@hotmail.com>
2022-10-17 17:41:31 +0200

    create post and open posts

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from .views import home, about
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView
+                    )
+
 
 urlpatterns = [
-    path('', home, name="blog-home"),
+    # path('', home, name="blog-home"),
     path('about/', about, name="blog-about"),
+    path('', PostListView.as_view(), name="blog-home"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/new/', PostCreateView.as_view(), name="post-create"),
 ]
 
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import PostsModel
+from django.views.generic import ListView, DetailView, CreateView
 # Create your views here.
 """
 MVC - MVT: Model View Template
@@ -19,3 +20,23 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html')
+
+# a modellt és a templétet meg kell adni
+class PostListView(ListView):
+    model = PostsModel
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = PostsModel
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(CreateView):
+    model = PostsModel
+    template_name = 'blog/post_create.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)

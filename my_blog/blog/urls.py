309b7e13c1173909ebd998a6e862de07eaa84fcc
ALamros <tikasz.zsolt.szeb@hotmail.com>
2022-10-24 19:56:31 +0200

    implement email sneding

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -4,13 +4,15 @@ from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
                     PostUpdateView,
-                    PostDeleteView
+                    PostDeleteView,
+                    UserPostListView
                     )
 
 
 urlpatterns = [
     # path('', home, name="blog-home"),
     path('about/', about, name="blog-about"),
+    path('user/<str:username>/', UserPostListView.as_view(), name="user-posts"),
     path('', PostListView.as_view(), name="blog-home"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
